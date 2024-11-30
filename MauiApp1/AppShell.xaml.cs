94a7aa98b3f0d14d294a1d62d0e3cd822d46351e
Rock Parker <rossyparker2@gmail.com>
2024-11-30 05:04:45 -0500

    Initial

---
+++ MauiApp1/AppShell.xaml.cs
@@ -0,0 +1,32 @@
+﻿namespace MauiApp1;
+
+public partial class AppShell : Shell
+{
+    private static bool IsFirstLaunch => true;
+    public AppShell()
+    {
+        InitializeComponent();
+        
+        HomeContent.ContentTemplate =  new DataTemplate(()=> new WelcomePage());
+        HomeContent.Route = "WelcomePage";
+    }
+
+    private void OnLaunch()
+    {
+        DataTemplate launchPage;
+        string route;
+        if (IsFirstLaunch)
+        {
+            launchPage = new DataTemplate(()=> new WelcomePage());
+            route = "WelcomePage";
+        }
+        else
+        {
+            launchPage = new DataTemplate(()=> new MainPage());
+            route = "MainPage";
+        }
+
+        HomeContent.ContentTemplate =  launchPage;
+        HomeContent.Route = route;
+    }
+}
---
+++ MauiApp1/SetupPages/WelcomePage.xaml.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MauiApp1;
+
+public partial class WelcomePage : ContentPage
+{
+    public WelcomePage()
+    {
+        InitializeComponent();
+    }
+    private void Setup_Clicked(object? sender, EventArgs e)
+    {
+        Navigation.PushAsync(new Setup());
+    }
+}
